7cd022f53ef5e451bfbca6f8d7a4c8862f59cba1
baptwild <baptiste.sauvage@hotmail.fr>
2023-03-16 09:42:39 +0100

    ajout regetration / profile basic

---
+++ src/views/auth/LoginView.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <h2> Bienvenue sur DisForce3 </h2>
+
+    <form @submit.prevent="login">
+      <div>
+        <label for="user_login"> Login </label>
+        <input type="text" id="user_login" v-model="user.login">
+      </div>
+      <div>
+        <label for="user_password"> Password </label>
+        <input type="text" id="user_password">
+      </div>
+      <div>
+        <button type="submit"> Connexion </button>
+      </div>
+
+    </form>
+    <div>
+      <button type="submit"> Inscription </button>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data(){
+    return{
+      user: {
+        login: '',
+        password: ''
+      }
+    }
+  },
+  methods:{
+    login(){
+      console.log('test pause')
+    }
+  }
+}
+
+</script>
---
+++ src/views/auth/Registration.vue
@@ -0,0 +1,34 @@
+<template>
+  <h2> Inscription </h2>
+
+  <form>
+    <div>
+      <label for="user_login"> Nom </label>
+      <input type="text" id="user_login" v-model="user.login">
+    </div>
+    <div>
+      <label for="user_login"> Prénom </label>
+      <input type="text" id="user_login" v-model="user.login">
+    </div>
+    <div>
+      <label for="user_login"> Login </label>
+      <input type="text" id="user_login" v-model="user.login">
+    </div>
+    <div>
+      <label for="user_password"> Password </label>
+      <input type="text" id="user_password">
+    </div>
+    <div>
+      <button type="submit"> S'inscrire  </button>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "Registration"
+}
+</script>
+
+<style scoped>
+
+</style>
